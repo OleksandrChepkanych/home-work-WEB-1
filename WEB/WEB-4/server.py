1ab917b473a1ce40c2837004be412a87f30e0966
OleksandrChepkanych <a.chepkanich@gmail.com>
2023-03-23 17:36:10 +0200

    upload

---
+++ WEB/WEB-4/server.py
@@ -0,0 +1,69 @@
+import pathlib
+import urllib.parse
+import mimetypes
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+base_dir = pathlib.Path()
+
+
+class App(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(urllib.parse.urlparse(self.path))
+        route = urllib.parse.urlparse(self.path)
+
+        match route.path:
+            case "/":
+                self.send_html("index.html")
+            case "/message.html":
+                self.send_html("message.html")
+            case _:
+                file = base_dir.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html("error.html", 404)
+
+    def send_html(self, filename, status_code=200):
+        self.send_response(status_code)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self, filename, status_code=200):
+        self.send_response(status_code)
+        mt = mimetypes.guess_type(filename)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def do_POST(self):
+        lenght = self.headers.get("Content-Length")
+        data = self.rfile.read(int(lenght))
+        self.save_data(data)
+        self.send_response(302)
+        self.send_header("Location", "message.html")
+        self.end_headers()
+        self.send_html("message.html")
+
+    def save_data(self, data):
+        parse_data = urllib.parse.unquote_plus(data.decode())
+        dict_parse = [el for el in parse_data.split("&")]
+        print(dict_parse)
+
+
+def run():
+    address = ("localhost", 3000)
+    httpd = HTTPServer(address, App)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+if __name__ == "__main__":
+    run()
---
+++ WEB/WEB-4/server_socket.py
@@ -0,0 +1,10 @@
+import socket
+
+
+def main():
+    host = socket.gethostname()
+    print(host)
+
+
+if __name__ == "__main__":
+    main()
